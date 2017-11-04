184d8c4853ec00541eff4adb992621645840777c
Hector Zumaeta Santiago <hector@emperitas.com>
2017-11-04 15:49:24 -0500

    Example of getting to active legislators by party and district

---
+++ etl.R
@@ -0,0 +1,9 @@
+library (tidyverse)
+library (data.table)
+
+legislators <- fread("tx_legislators.csv") %>% filter (active==TRUE)
+rolesandparty <- fread("tx_legislator_roles.csv") %>% filter (type=="member") %>%
+  group_by(leg_id) %>% filter (term==max(term))
+
+legisfinal <- legislators %>% left_join(rolesandparty, by="leg_id") %>% filter (type=="member")
+
